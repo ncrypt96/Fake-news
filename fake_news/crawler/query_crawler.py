2325971ea51ade55ad41dabf5ec51a59b9a014ee
Nithin Stephen <nithinstephen1996@gmail.com>
2019-01-27 00:30:53 +0530

    crawler class constructor initialized

--- fake_news/crawler/query_crawler.py
+++ fake_news/crawler/query_crawler.py
@@ -1,21 +1,44 @@
 from newspaper import Article
-from urllib.parse import urlparse
 from newsapi import NewsApiClient
 import requests
 
 class Crawler:
+    """
+    This class contains all the methods to extract the content from the given link
+    """
 
-    def check_if_website_exists(self,URL):
+    def __init__(self,URL):
+        """
+        This method initializes the URL in the class
+
+        :type URL: string
+        :param URL: The URL of the Website
+        """
+        self.URL = URL
 
         try:
-
-            requests = requests.get(URL)
-
+            article = Article(self.URL)
         except Exception as exception:
+            print("There was a problem while initializing the article object: {}".format(exception))
+        
+        try:
+            article.download()
+        except Exception as exception:
+            print("There was a problem while downloading the article: {}".format(exception))
 
-            print(exception)
+        try:
+            article.parse()
+        except Exception as exception:
+            print("There was a problem while parsing the article: {}".format(exception))
+        
 
-            raise 
+    def get_title(self):
+        """
+        This method returns the title from the given URL 
+        """
+        
+        
+
 
 
 
