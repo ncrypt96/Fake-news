701f65d0246be2e5bbcfe0659e6665725db4e8c0
Nithin Stephen <nithinstephen1996@gmail.com>
2019-01-27 16:50:05 +0530

    Class NewsApiHandle almost done

--- fake_news/crawler/query_crawler.py
+++ fake_news/crawler/query_crawler.py
@@ -1,6 +1,7 @@
 from newspaper import Article
 from newsapi import NewsApiClient
 import requests
+import json
 
 class Crawler:
     """
@@ -64,46 +65,73 @@ class NewsApiHandle:
     """
     This class contains methods to handle everything related to the use of news api
     """
-    def __init__(self,API_Key):
+    def __init__(self,API_Key,keyword_list):
         """
-        This method inititalizes the news api
-        """
-
-        #initialize news client with the api key
-        self.news_api = NewsApiClient(api_key=API_Key)
-
-    def request_news_URLs(self,keyword_list):
-
-        """
-        This method takes in a list of keywords as the argument and applies AND operation between them and queries it 
-        with the help of news api and returns a maximum of 5 related Urls (list)
+        This method inititalizes the news api and also takes in a list of keywords as the argument and applies AND operation between them and queries it 
+        and stores it in the variable response
 
         :type keyword_list: list
         :param keyword_list: list of keywords to query the api
         """
 
+        #initialize news client with the api key
+        self.news_api = NewsApiClient(api_key=API_Key)
+
         # the sting to be appended in the middle
         AND = " AND "
-
-        # initialize an empty list which will hold the URLs
-        list_of_URLs =[]
         
         # add AND in between the keywords in the list
         query_string = AND.join(keyword_list)
 
+        # initialize an empty list of titles
+        self.title_list = []
+
+        # initialize an empty list of meta descriptions
+        self.descriptions_list = []
+
+        # initialize an empty list of Urls
+        self.Urls_list = []
+
         # query the api
-        all_articles = self.news_api.get_everything(q=query_string)
+        response = self.news_api.get_everything(q=query_string)
 
-        # get the number of articles given by the api
+        # if the size of list_of_URLs is more then 5 set parse_length to 5 else according to its size
+        parse_length = 5  if len(response['articles']) >= 5 else len(response['articles'][0])
+
         # for each article returned get the corresponding URL and append it to list_of_URLs
-        # if the size of list_of_URLs is more than 5 break the loop and return the URLs
-        for i in range(0,len(all_articles['articles'])):
-            list_of_URLs.append(all_articles["articles"][i]["url"])
+        for item in range(0,parse_length):
 
-            if len(all_articles) > 5:
-                break
+            # append every title to title list
+            self.title_list.append(response["articles"][item]["title"])
 
-        return list_of_URLs
+            # append every description to description_list
+            self.descriptions_list.append(response["articles"][item]["description"])
+
+            # append every Urls to Urls_list
+            self.Urls_list.append(response["articles"][item]["url"])
+
+
+
+    def get_URLs(self):
+        """
+        This method returns a maximum of 5 news Urls to extract content from (list)
+        """
+        return self.Urls_list
+
+    def get_titles(self):
+        """
+        This method returns a maximum of 5 news titles (list)
+        """
+        return self.title_list
+
+    def get_descriptions(self):
+        """
+        This method returns a maximum of 5 descriptions (list)
+        """
+        return self.descriptions_list
+        
+
+
 
 
 
