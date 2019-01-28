5ce8f9f450891ae154293d3111a15c1310f2168a
Nithin Stephen <nithinstephen1996@gmail.com>
2019-01-29 02:37:50 +0530

    added more methods for keyword extraction

--- fake_news/preprocessor/keywords_check.py
+++ fake_news/preprocessor/keywords_check.py
@@ -76,4 +76,69 @@ class KeyWordCheck:
         #---------------------------------------
         return set(final_keyword_list)
 
+
+    def keyword_reducer(self,keywords):
+        """
+        This method removes the repetitive keywords and reduces them to a single entity
+
+        :type keywords: list
+        :param keywords: the keywords to be reduced
+        """
+        # initialize a empty list
+        lower_case_keywords = []
+
+        # for every word make it lower case and append
+        for word in keywords:
+            lower_case_keywords.append(word.lower())
+
+        # make a sorted list 
+        # sort the keywords according to their length
+        sorted_keywords = sorted(lower_case_keywords,key=len)
+
+        # if small substring contain in other big string remove all the big strings associated with it
+        for word in sorted_keywords:
+
+            for item in sorted_keywords:
+
+                if(word!=item):
+
+                    if(word in item):
+
+                        sorted_keywords.remove(item)
+
+        return sorted_keywords
+
+    
+    def eir_keywords(self,text):
+        """
+        This method uses named entity recognition for the extraction of keywords from the given text
+        returns a list of keywords
+
+        :type text: string
+        :param text: The text from where keywords need to be extracted
+        """
+        #initialize empty
+        keywords_list = []
+
+        # load the english module
+        # install 'en' model (python3 -m spacy download en)
+        nlp = spacy.load('en')
+        
+        # process the doc
+        doc  = nlp(text)
+
+        # get the entities 
+        keywords = doc.ents
+
+        # convert each entity to string and apend
+        for word in keywords:
+
+            keywords_list.append(str(word))
+        
+        return keywords_list
+
+
+
+
+
                     
