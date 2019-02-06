25c7237925c8fc54284d86b82cf2327d27046c3c
Nithin Stephen <nithinstephen1996@gmail.com>
2019-02-07 03:55:00 +0530

    improved keyword extraction

--- fake_news/crawler/query_data.py
+++ fake_news/crawler/query_data.py
@@ -16,14 +16,14 @@ class Data:
         self.URL = URL
         self.user_news_crawler = Crawler(URL)
         
-    def get_all_data(self):
+    def get_all_data(self,no_of_keywords=6):
         """
         This method returns all the data in the form of a python dictionary which can be converted inti json
         passing the parameter ['all'] in the returns all of the information
         passing the parameters like ['titles'], ['descriptions'], ['contents'] return that specific data
 
-        :type keyword_extraction_algo: string
-        :param keyword_extraction_algo : The algorithm to be used to extract the keywords
+        :type no_of_keywords: int
+        :param no_of_keywords : the number of keywords considered
         """
         # initialize NewApi object
         
@@ -46,28 +46,69 @@ class Data:
         #---------------------------------------------------
         #-------------keyword processing START--------------
 
-        # initialize an empty list 
-        keywords = []
         # use to get unique kewords to query the api
         keywords_manager = KeyWordCheck()
 
-        intermediate_keywords = user_news_meta_keywords
+        # initialize an empty list 
+        keywords = []
+
+        intermediate_keywords = sorted((list(set(user_news_meta_keywords))),key=len)
 
         # for named enitities as keywords
         eir_keywords = []
 
-        if(len(intermediate_keywords)<=3):
+        # if the length of the meta keywords is less tha or equal to 2
+        if(len(user_news_meta_keywords)<=1):
+            no_meta_eir_keywords = list(set(keywords_manager.eir_keywords(user_news_meta_description) + keywords_manager.eir_keywords(user_news_title)))
+            # lower case and sort
+            print(no_meta_eir_keywords)
+            no_meta_eir_keywords = keywords_manager.keyword_formatter(no_meta_eir_keywords)
+            print(no_meta_eir_keywords)
+            # reduce smilar keywords
+            no_meta_eir_keywords = keywords_manager.keyword_reducer(no_meta_eir_keywords)
+            print(no_meta_eir_keywords)
+            # remove irrelevent
+            no_meta_eir_keywords = keywords_manager.remove_irrelevant_keywords(no_meta_eir_keywords)
+            print("here 13")
+            line_loc()
+            print(no_meta_eir_keywords)
+            # check if there are more than one keywords but less than 3
+            if(len(no_meta_eir_keywords)>1 and len(no_meta_eir_keywords)<=no_of_keywords):
+                keywords = no_meta_eir_keywords
+                print("here 14")
+                line_loc()
+            elif(len(no_meta_eir_keywords)==(no_of_keywords+1)):
+                no_meta_eir_keywords.pop(-1)
+                keywords = no_meta_eir_keywords
+                print("here 15")
+                line_loc()
+            elif(len(no_meta_eir_keywords)==(no_of_keywords+2)):
+                no_meta_eir_keywords.pop(-1)
+                no_meta_eir_keywords.pop(-1)
+                keywords = no_meta_eir_keywords
+                print("here 16")
+                line_loc()
+            else:
+                print("here 17")
+                highlight_back("There was a problem while extracting the keywords",'R')
+                highlight_fore("Please input unique keywords relevent to the article separated by ','","B")
+                highlight_fore("Suggested Keywords: ",'Y')
+                highlight_fore(list(set(no_meta_eir_keywords)),'G')
+                # get keywords from the user
+                keywords = input().split(',') 
+                line_loc()       
+        elif(len(intermediate_keywords)>=2 and len(intermediate_keywords)<=no_of_keywords):
             # assign meta keywords to keywords
             keywords = intermediate_keywords
             print("here 1")
             line_loc()
-        elif(len(intermediate_keywords)==4):
+        elif(len(intermediate_keywords)==(no_of_keywords+1)):
             # pop the last element from the list
             intermediate_keywords.pop(-1)
             keywords = intermediate_keywords
             print("here 2")
             line_loc()            
-        elif(len(intermediate_keywords)==5):
+        elif(len(intermediate_keywords)==(no_of_keywords+2)):
             intermediate_keywords.pop(-1)
             intermediate_keywords.pop(-1)
             keywords = intermediate_keywords
@@ -81,16 +122,16 @@ class Data:
             # remove irrevelent keywords which are put for seo purposes
             intermediate_keywords = keywords_manager.remove_irrelevant_keywords(intermediate_keywords)
             # check if now keywords are less than or equal to 4
-            if(len(intermediate_keywords)<=3):
+            if(len(intermediate_keywords)<=no_of_keywords):
                 keywords = intermediate_keywords
                 print("here 4")
                 line_loc()
-            elif(len(intermediate_keywords)==4):
+            elif(len(intermediate_keywords)==(no_of_keywords+1)):
                 intermediate_keywords.pop(-1)
                 keywords = intermediate_keywords
                 print("here 5")
                 line_loc()
-            elif(len(intermediate_keywords)==5):
+            elif(len(intermediate_keywords)==(no_of_keywords+2)):
                 intermediate_keywords.pop(-1)
                 intermediate_keywords.pop(-1)
                 keywords = intermediate_keywords
@@ -112,16 +153,16 @@ class Data:
                 line_loc()
                 print(eir_keywords)
                 # check if there are more than one keywords but less than 3
-                if(len(eir_keywords)>1 and len(eir_keywords)<=3):
+                if(len(eir_keywords)>1 and len(eir_keywords)<=no_of_keywords):
                     keywords = eir_keywords
                     print("here 8")
                     line_loc()
-                elif(len(eir_keywords)==4):
+                elif(len(eir_keywords)==(no_of_keywords+1)):
                     eir_keywords.pop(-1)
                     keywords = eir_keywords
                     print("here 9")
                     line_loc()
-                elif(len(eir_keywords)==5):
+                elif(len(eir_keywords)==(no_of_keywords+2)):
                     eir_keywords.pop(-1)
                     eir_keywords.pop(-1)
                     keywords = eir_keywords
@@ -141,6 +182,16 @@ class Data:
                         highlight_fore(list(set(intermediate_keywords+eir_keywords)),'G')
                         # get keywords from the user
                         keywords = input().split(',')
+                    elif(len(keywords)>no_of_keywords):
+                        print("here 12")
+                        line_loc()
+                        highlight_back("There was a problem while extracting the keywords",'R')
+                        highlight_fore("Please input unique keywords relevent to the article separated by ','","B")
+                        highlight_fore("Suggested Keywords: ",'Y')
+                        highlight_fore(list(set(intermediate_keywords+eir_keywords)),'G')
+                        # get keywords from the user
+                        keywords = input().split(',')
+
 
         #-------------keyword processing END--------------
         print(keywords)
